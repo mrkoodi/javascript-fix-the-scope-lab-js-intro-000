7ad4d31215b8aeeb7e439309548d86fec69cebd9
Ebenezer Koodi <mrrkoodi@gmail.com>
2018-01-09 20:15:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var animal = 'dog'
-
+const two = 2
 function myAnimal() {
   return animal
 }
@@ -13,10 +13,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
